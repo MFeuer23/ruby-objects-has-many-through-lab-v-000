f843991b0c5377e0ac06a6c749ab9c779853a262
Mallory Feuer <mallory.feuer@gmail.com>
2018-02-23 22:34:58 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,12 +1,13 @@
 class Genre
 
-def initialize(name)
-  @name = name
-  @songs = []
-end
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
 
-def add_song(song)
-  @songs << song
-  song.genre = self
+  def add_song(song)
+    @songs << song
+    song.genre = self
+  end
 
 end
